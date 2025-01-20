523b6cff7b18dedce3e1b7fecef353507ad6bca9
swapnilahmedshishir <ahmed66882298@gmail.com>
2025-01-20 20:40:20 +0600

    update the react router dom file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RoutersItems from "./Component/Router/Router.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(RoutersItems, {
+  future: {
+    v7_startTransition: true,
+    v7_normalizeFormMethod: true,
+  },
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
